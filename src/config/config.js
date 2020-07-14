c89dc640b9a61853650c38ff851a8e901d23aa14
wulon <wuliang_5016@163.com>
2020-07-14 14:58:17 +0800

    Merge branch 'master' of gitee.com:wulon/mk-teamwork-ui 1、提交开源版本

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export default {
-    crossDomain: false, //是否开启跨域支持
-    PROD_URL: 'http://127.0.0.1:8888/api', //生产环境接口地址
-    //PROD_URL: 'http://120.27.62.173:8111/api', //生产环境接口地址
-    WS_URI: 'ws://127.0.0.1:8888',// wss://beta.vilson.xyz:2345 //WebSocket地址
+    crossDomain: true, //是否开启跨域支持
+    //PROD_URL: 'http://teamwork.mokingsoft.com/api', //生产环境接口地址
+    PROD_URL: 'http://127.0.0.1:8888', //开发环境接口地址
+    WS_URI: 'ws://127.0.0.1:8888',//wss://beta.vilson.xyz:2345 //WebSocket地址
     HOME_PAGE: '/home',//主页路由
 };
